2a1f79b4a2b419dcf14b0a800af8c6f286122c05
chemzqm <chemzqm@gmail.com>
2014-06-04 12:32:16 +0800

    Update documentation

---
+++ build/build.js
@@ -0,0 +1,706 @@
+/**
+ * Require the module at `name`.
+ *
+ * @param {String} name
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(name) {
+  var module = require.modules[name];
+  if (!module) throw new Error('failed to require "' + name + '"');
+
+  if (!('exports' in module) && typeof module.definition === 'function') {
+    module.client = module.component = true;
+    module.definition.call(this, module.exports = {}, module);
+    delete module.definition;
+  }
+
+  return module.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Register module at `name` with callback `definition`.
+ *
+ * @param {String} name
+ * @param {Function} definition
+ * @api private
+ */
+
+require.register = function (name, definition) {
+  require.modules[name] = {
+    definition: definition
+  };
+};
+
+/**
+ * Define a module's exports immediately with `exports`.
+ *
+ * @param {String} name
+ * @param {Generic} exports
+ * @api private
+ */
+
+require.define = function (name, exports) {
+  require.modules[name] = {
+    exports: exports
+  };
+};
+require.register("component~raf@1.1.3", Function("exports, module",
+"/**\n\
+ * Expose `requestAnimationFrame()`.\n\
+ */\n\
+\n\
+exports = module.exports = window.requestAnimationFrame\n\
+  || window.webkitRequestAnimationFrame\n\
+  || window.mozRequestAnimationFrame\n\
+  || window.oRequestAnimationFrame\n\
+  || window.msRequestAnimationFrame\n\
+  || fallback;\n\
+\n\
+/**\n\
+ * Fallback implementation.\n\
+ */\n\
+\n\
+var prev = new Date().getTime();\n\
+function fallback(fn) {\n\
+  var curr = new Date().getTime();\n\
+  var ms = Math.max(0, 16 - (curr - prev));\n\
+  var req = setTimeout(fn, ms);\n\
+  prev = curr;\n\
+  return req;\n\
+}\n\
+\n\
+/**\n\
+ * Cancel.\n\
+ */\n\
+\n\
+var cancel = window.cancelAnimationFrame\n\
+  || window.webkitCancelAnimationFrame\n\
+  || window.mozCancelAnimationFrame\n\
+  || window.oCancelAnimationFrame\n\
+  || window.msCancelAnimationFrame\n\
+  || window.clearTimeout;\n\
+\n\
+exports.cancel = function(id){\n\
+  cancel.call(window, id);\n\
+};\n\
+\n\
+//# sourceURL=components/component/raf/1.1.3/index.js"
+));
+
+require.modules["component-raf"] = require.modules["component~raf@1.1.3"];
+require.modules["component~raf"] = require.modules["component~raf@1.1.3"];
+require.modules["raf"] = require.modules["component~raf@1.1.3"];
+
+
+require.register("component~emitter@1.0.0", Function("exports, module",
+"\n\
+/**\n\
+ * Expose `Emitter`.\n\
+ */\n\
+\n\
+module.exports = Emitter;\n\
+\n\
+/**\n\
+ * Initialize a new `Emitter`.\n\
+ *\n\
+ * @api public\n\
+ */\n\
+\n\
+function Emitter(obj) {\n\
+  if (obj) return mixin(obj);\n\
+};\n\
+\n\
+/**\n\
+ * Mixin the emitter properties.\n\
+ *\n\
+ * @param {Object} obj\n\
+ * @return {Object}\n\
+ * @api private\n\
+ */\n\
+\n\
+function mixin(obj) {\n\
+  for (var key in Emitter.prototype) {\n\
+    obj[key] = Emitter.prototype[key];\n\
+  }\n\
+  return obj;\n\
+}\n\
+\n\
+/**\n\
+ * Listen on the given `event` with `fn`.\n\
+ *\n\
+ * @param {String} event\n\
+ * @param {Function} fn\n\
+ * @return {Emitter}\n\
+ * @api public\n\
+ */\n\
+\n\
+Emitter.prototype.on = function(event, fn){\n\
+  this._callbacks = this._callbacks || {};\n\
+  (this._callbacks[event] = this._callbacks[event] || [])\n\
+    .push(fn);\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Adds an `event` listener that will be invoked a single\n\
+ * time then automatically removed.\n\
+ *\n\
+ * @param {String} event\n\
+ * @param {Function} fn\n\
+ * @return {Emitter}\n\
+ * @api public\n\
+ */\n\
+\n\
+Emitter.prototype.once = function(event, fn){\n\
+  var self = this;\n\
+  this._callbacks = this._callbacks || {};\n\
+\n\
+  function on() {\n\
+    self.off(event, on);\n\
+    fn.apply(this, arguments);\n\
+  }\n\
+\n\
+  fn._off = on;\n\
+  this.on(event, on);\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Remove the given callback for `event` or all\n\
+ * registered callbacks.\n\
+ *\n\
+ * @param {String} event\n\
+ * @param {Function} fn\n\
+ * @return {Emitter}\n\
+ * @api public\n\
+ */\n\
+\n\
+Emitter.prototype.off =\n\
+Emitter.prototype.removeListener =\n\
+Emitter.prototype.removeAllListeners = function(event, fn){\n\
+  this._callbacks = this._callbacks || {};\n\
+\n\
+  // all\n\
+  if (0 == arguments.length) {\n\
+    this._callbacks = {};\n\
+    return this;\n\
+  }\n\
+\n\
+  // specific event\n\
+  var callbacks = this._callbacks[event];\n\
+  if (!callbacks) return this;\n\
+\n\
+  // remove all handlers\n\
+  if (1 == arguments.length) {\n\
+    delete this._callbacks[event];\n\
+    return this;\n\
+  }\n\
+\n\
+  // remove specific handler\n\
+  var i = callbacks.indexOf(fn._off || fn);\n\
+  if (~i) callbacks.splice(i, 1);\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Emit `event` with the given args.\n\
+ *\n\
+ * @param {String} event\n\
+ * @param {Mixed} ...\n\
+ * @return {Emitter}\n\
+ */\n\
+\n\
+Emitter.prototype.emit = function(event){\n\
+  this._callbacks = this._callbacks || {};\n\
+  var args = [].slice.call(arguments, 1)\n\
+    , callbacks = this._callbacks[event];\n\
+\n\
+  if (callbacks) {\n\
+    callbacks = callbacks.slice(0);\n\
+    for (var i = 0, len = callbacks.length; i < len; ++i) {\n\
+      callbacks[i].apply(this, args);\n\
+    }\n\
+  }\n\
+\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Return array of callbacks for `event`.\n\
+ *\n\
+ * @param {String} event\n\
+ * @return {Array}\n\
+ * @api public\n\
+ */\n\
+\n\
+Emitter.prototype.listeners = function(event){\n\
+  this._callbacks = this._callbacks || {};\n\
+  return this._callbacks[event] || [];\n\
+};\n\
+\n\
+/**\n\
+ * Check if this emitter has `event` handlers.\n\
+ *\n\
+ * @param {String} event\n\
+ * @return {Boolean}\n\
+ * @api public\n\
+ */\n\
+\n\
+Emitter.prototype.hasListeners = function(event){\n\
+  return !! this.listeners(event).length;\n\
+};\n\
+\n\
+//# sourceURL=components/component/emitter/1.0.0/index.js"
+));
+
+require.modules["component-emitter"] = require.modules["component~emitter@1.0.0"];
+require.modules["component~emitter"] = require.modules["component~emitter@1.0.0"];
+require.modules["emitter"] = require.modules["component~emitter@1.0.0"];
+
+
+require.register("component~ease@1.0.0", Function("exports, module",
+"\n\
+exports.linear = function(n){\n\
+  return n;\n\
+};\n\
+\n\
+exports.inQuad = function(n){\n\
+  return n * n;\n\
+};\n\
+\n\
+exports.outQuad = function(n){\n\
+  return n * (2 - n);\n\
+};\n\
+\n\
+exports.inOutQuad = function(n){\n\
+  n *= 2;\n\
+  if (n < 1) return 0.5 * n * n;\n\
+  return - 0.5 * (--n * (n - 2) - 1);\n\
+};\n\
+\n\
+exports.inCube = function(n){\n\
+  return n * n * n;\n\
+};\n\
+\n\
+exports.outCube = function(n){\n\
+  return --n * n * n + 1;\n\
+};\n\
+\n\
+exports.inOutCube = function(n){\n\
+  n *= 2;\n\
+  if (n < 1) return 0.5 * n * n * n;\n\
+  return 0.5 * ((n -= 2 ) * n * n + 2);\n\
+};\n\
+\n\
+exports.inQuart = function(n){\n\
+  return n * n * n * n;\n\
+};\n\
+\n\
+exports.outQuart = function(n){\n\
+  return 1 - (--n * n * n * n);\n\
+};\n\
+\n\
+exports.inOutQuart = function(n){\n\
+  n *= 2;\n\
+  if (n < 1) return 0.5 * n * n * n * n;\n\
+  return -0.5 * ((n -= 2) * n * n * n - 2);\n\
+};\n\
+\n\
+exports.inQuint = function(n){\n\
+  return n * n * n * n * n;\n\
+}\n\
+\n\
+exports.outQuint = function(n){\n\
+  return --n * n * n * n * n + 1;\n\
+}\n\
+\n\
+exports.inOutQuint = function(n){\n\
+  n *= 2;\n\
+  if (n < 1) return 0.5 * n * n * n * n * n;\n\
+  return 0.5 * ((n -= 2) * n * n * n * n + 2);\n\
+};\n\
+\n\
+exports.inSine = function(n){\n\
+  return 1 - Math.cos(n * Math.PI / 2 );\n\
+};\n\
+\n\
+exports.outSine = function(n){\n\
+  return Math.sin(n * Math.PI / 2);\n\
+};\n\
+\n\
+exports.inOutSine = function(n){\n\
+  return .5 * (1 - Math.cos(Math.PI * n));\n\
+};\n\
+\n\
+exports.inExpo = function(n){\n\
+  return 0 == n ? 0 : Math.pow(1024, n - 1);\n\
+};\n\
+\n\
+exports.outExpo = function(n){\n\
+  return 1 == n ? n : 1 - Math.pow(2, -10 * n);\n\
+};\n\
+\n\
+exports.inOutExpo = function(n){\n\
+  if (0 == n) return 0;\n\
+  if (1 == n) return 1;\n\
+  if ((n *= 2) < 1) return .5 * Math.pow(1024, n - 1);\n\
+  return .5 * (-Math.pow(2, -10 * (n - 1)) + 2);\n\
+};\n\
+\n\
+exports.inCirc = function(n){\n\
+  return 1 - Math.sqrt(1 - n * n);\n\
+};\n\
+\n\
+exports.outCirc = function(n){\n\
+  return Math.sqrt(1 - (--n * n));\n\
+};\n\
+\n\
+exports.inOutCirc = function(n){\n\
+  n *= 2\n\
+  if (n < 1) return -0.5 * (Math.sqrt(1 - n * n) - 1);\n\
+  return 0.5 * (Math.sqrt(1 - (n -= 2) * n) + 1);\n\
+};\n\
+\n\
+exports.inBack = function(n){\n\
+  var s = 1.70158;\n\
+  return n * n * (( s + 1 ) * n - s);\n\
+};\n\
+\n\
+exports.outBack = function(n){\n\
+  var s = 1.70158;\n\
+  return --n * n * ((s + 1) * n + s) + 1;\n\
+};\n\
+\n\
+exports.inOutBack = function(n){\n\
+  var s = 1.70158 * 1.525;\n\
+  if ( ( n *= 2 ) < 1 ) return 0.5 * ( n * n * ( ( s + 1 ) * n - s ) );\n\
+  return 0.5 * ( ( n -= 2 ) * n * ( ( s + 1 ) * n + s ) + 2 );\n\
+};\n\
+\n\
+exports.inBounce = function(n){\n\
+  return 1 - exports.outBounce(1 - n);\n\
+};\n\
+\n\
+exports.outBounce = function(n){\n\
+  if ( n < ( 1 / 2.75 ) ) {\n\
+    return 7.5625 * n * n;\n\
+  } else if ( n < ( 2 / 2.75 ) ) {\n\
+    return 7.5625 * ( n -= ( 1.5 / 2.75 ) ) * n + 0.75;\n\
+  } else if ( n < ( 2.5 / 2.75 ) ) {\n\
+    return 7.5625 * ( n -= ( 2.25 / 2.75 ) ) * n + 0.9375;\n\
+  } else {\n\
+    return 7.5625 * ( n -= ( 2.625 / 2.75 ) ) * n + 0.984375;\n\
+  }\n\
+};\n\
+\n\
+exports.inOutBounce = function(n){\n\
+  if (n < .5) return exports.inBounce(n * 2) * .5;\n\
+  return exports.outBounce(n * 2 - 1) * .5 + .5;\n\
+};\n\
+\n\
+// aliases\n\
+\n\
+exports['in-quad'] = exports.inQuad;\n\
+exports['out-quad'] = exports.outQuad;\n\
+exports['in-out-quad'] = exports.inOutQuad;\n\
+exports['in-cube'] = exports.inCube;\n\
+exports['out-cube'] = exports.outCube;\n\
+exports['in-out-cube'] = exports.inOutCube;\n\
+exports['in-quart'] = exports.inQuart;\n\
+exports['out-quart'] = exports.outQuart;\n\
+exports['in-out-quart'] = exports.inOutQuart;\n\
+exports['in-quint'] = exports.inQuint;\n\
+exports['out-quint'] = exports.outQuint;\n\
+exports['in-out-quint'] = exports.inOutQuint;\n\
+exports['in-sine'] = exports.inSine;\n\
+exports['out-sine'] = exports.outSine;\n\
+exports['in-out-sine'] = exports.inOutSine;\n\
+exports['in-expo'] = exports.inExpo;\n\
+exports['out-expo'] = exports.outExpo;\n\
+exports['in-out-expo'] = exports.inOutExpo;\n\
+exports['in-circ'] = exports.inCirc;\n\
+exports['out-circ'] = exports.outCirc;\n\
+exports['in-out-circ'] = exports.inOutCirc;\n\
+exports['in-back'] = exports.inBack;\n\
+exports['out-back'] = exports.outBack;\n\
+exports['in-out-back'] = exports.inOutBack;\n\
+exports['in-bounce'] = exports.inBounce;\n\
+exports['out-bounce'] = exports.outBounce;\n\
+exports['in-out-bounce'] = exports.inOutBounce;\n\
+\n\
+//# sourceURL=components/component/ease/1.0.0/index.js"
+));
+
+require.modules["component-ease"] = require.modules["component~ease@1.0.0"];
+require.modules["component~ease"] = require.modules["component~ease@1.0.0"];
+require.modules["ease"] = require.modules["component~ease@1.0.0"];
+
+
+require.register("component~tween@1.1.0", Function("exports, module",
+"\n\
+/**\n\
+ * Module dependencies.\n\
+ */\n\
+\n\
+var Emitter = require(\"component~emitter@1.0.0\")\n\
+  , ease = require(\"component~ease@1.0.0\");\n\
+\n\
+/**\n\
+ * Expose `Tween`.\n\
+ */\n\
+\n\
+module.exports = Tween;\n\
+\n\
+/**\n\
+ * Initialize a new `Tween` with `obj`.\n\
+ *\n\
+ * @param {Object|Array} obj\n\
+ * @api public\n\
+ */\n\
+\n\
+function Tween(obj) {\n\
+  if (!(this instanceof Tween)) return new Tween(obj);\n\
+  this._from = obj;\n\
+  this.ease('linear');\n\
+  this.duration(500);\n\
+}\n\
+\n\
+/**\n\
+ * Mixin emitter.\n\
+ */\n\
+\n\
+Emitter(Tween.prototype);\n\
+\n\
+/**\n\
+ * Reset the tween.\n\
+ *\n\
+ * @api public\n\
+ */\n\
+\n\
+Tween.prototype.reset = function(){\n\
+  this.isArray = Array.isArray(this._from);\n\
+  this._curr = clone(this._from);\n\
+  this._done = false;\n\
+  this._start = Date.now();\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Tween to `obj` and reset internal state.\n\
+ *\n\
+ *    tween.to({ x: 50, y: 100 })\n\
+ *\n\
+ * @param {Object|Array} obj\n\
+ * @return {Tween} self\n\
+ * @api public\n\
+ */\n\
+\n\
+Tween.prototype.to = function(obj){\n\
+  this.reset();\n\
+  this._to = obj;\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Set duration to `ms` [500].\n\
+ *\n\
+ * @param {Number} ms\n\
+ * @return {Tween} self\n\
+ * @api public\n\
+ */\n\
+\n\
+Tween.prototype.duration = function(ms){\n\
+  this._duration = ms;\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Set easing function to `fn`.\n\
+ *\n\
+ *    tween.ease('in-out-sine')\n\
+ *\n\
+ * @param {String|Function} fn\n\
+ * @return {Tween}\n\
+ * @api public\n\
+ */\n\
+\n\
+Tween.prototype.ease = function(fn){\n\
+  fn = 'function' == typeof fn ? fn : ease[fn];\n\
+  if (!fn) throw new TypeError('invalid easing function');\n\
+  this._ease = fn;\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Stop the tween and immediately emit \"stop\" and \"end\".\n\
+ *\n\
+ * @return {Tween}\n\
+ * @api public\n\
+ */\n\
+\n\
+Tween.prototype.stop = function(){\n\
+  this.stopped = true;\n\
+  this._done = true;\n\
+  this.emit('stop');\n\
+  this.emit('end');\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Perform a step.\n\
+ *\n\
+ * @return {Tween} self\n\
+ * @api private\n\
+ */\n\
+\n\
+Tween.prototype.step = function(){\n\
+  if (this._done) return;\n\
+\n\
+  // duration\n\
+  var duration = this._duration;\n\
+  var now = Date.now();\n\
+  var delta = now - this._start;\n\
+  var done = delta >= duration;\n\
+\n\
+  // complete\n\
+  if (done) {\n\
+    this._from = this._to;\n\
+    this._update(this._to);\n\
+    this._done = true;\n\
+    this.emit('end');\n\
+    return this;\n\
+  }\n\
+\n\
+  // tween\n\
+  var from = this._from;\n\
+  var to = this._to;\n\
+  var curr = this._curr;\n\
+  var fn = this._ease;\n\
+  var p = (now - this._start) / duration;\n\
+  var n = fn(p);\n\
+\n\
+  // array\n\
+  if (this.isArray) {\n\
+    for (var i = 0; i < from.length; ++i) {\n\
+      curr[i] = from[i] + (to[i] - from[i]) * n;\n\
+    }\n\
+\n\
+    this._update(curr);\n\
+    return this;\n\
+  }\n\
+\n\
+  // objech\n\
+  for (var k in from) {\n\
+    curr[k] = from[k] + (to[k] - from[k]) * n;\n\
+  }\n\
+\n\
+  this._update(curr);\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Set update function to `fn` or\n\
+ * when no argument is given this performs\n\
+ * a \"step\".\n\
+ *\n\
+ * @param {Function} fn\n\
+ * @return {Tween} self\n\
+ * @api public\n\
+ */\n\
+\n\
+Tween.prototype.update = function(fn){\n\
+  if (0 == arguments.length) return this.step();\n\
+  this._update = fn;\n\
+  return this;\n\
+};\n\
+\n\
+/**\n\
+ * Clone `obj`.\n\
+ *\n\
+ * @api private\n\
+ */\n\
+\n\
+function clone(obj) {\n\
+  if (Array.isArray(obj)) return obj.slice();\n\
+  var ret = {};\n\
+  for (var key in obj) ret[key] = obj[key];\n\
+  return ret;\n\
+}\n\
+\n\
+//# sourceURL=components/component/tween/1.1.0/index.js"
+));
+
+require.modules["component-tween"] = require.modules["component~tween@1.1.0"];
+require.modules["component~tween"] = require.modules["component~tween@1.1.0"];
+require.modules["tween"] = require.modules["component~tween@1.1.0"];
+
+
+require.register("scroll", Function("exports, module",
+"var raf = require(\"component~raf@1.1.3\");\n\
+var Tween = require(\"component~tween@1.1.0\");\n\
+\n\
+function Scroll(el, opts) {\n\
+  if (!(this instanceof Scroll)) return new Scroll(el, opts);\n\
+  opts = opts || {};\n\
+  this.el = el;\n\
+  this.ease = opts.ease || 'out-circ';\n\
+  this.duration = opts.duration || 1000;\n\
+}\n\
+\n\
+Scroll.prototype.left = function (n) {\n\
+  this._left = n;\n\
+  return this;\n\
+}\n\
+\n\
+Scroll.prototype.top = function (n) {\n\
+  this._top = n;\n\
+  return this;\n\
+}\n\
+\n\
+Scroll.prototype.end = function (cb) {\n\
+  var o = {};\n\
+  var el = this.el;\n\
+  var l = (typeof this._left !== 'undefined');\n\
+  var t = (typeof this._top !== 'undefined');\n\
+  if (l) o.left = el.scrollLeft;\n\
+  if (t) o.top = el.scrollTop;\n\
+  var des = { left: this._left, top: this._top };\n\
+  var tween = Tween(o)\n\
+    .ease(this.ease)\n\
+    .to(des)\n\
+    .duration(this.duration);\n\
+\n\
+  tween.update(function(o){\n\
+    if (l) el.scrollLeft = o.left;\n\
+    if (t) el.scrollTop = o.top;\n\
+  });\n\
+\n\
+  tween.on('end', function(){\n\
+    animate = function(){};\n\
+    if (cb) cb();\n\
+  });\n\
+\n\
+  function animate() {\n\
+    raf(animate);\n\
+    tween.update();\n\
+  }\n\
+\n\
+  animate();\n\
+}\n\
+\n\
+module.exports = Scroll;\n\
+\n\
+//# sourceURL=index.js"
+));
+
+require.modules["scroll"] = require.modules["scroll"];
+
+
